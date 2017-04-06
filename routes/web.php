42586c93243048e985b528d4a42957e8c426f7aa
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2017-04-06 14:22:52 +0200

    Sync

--- routes/web.php
+++ routes/web.php
@@ -22,4 +22,8 @@ Route::group(['middleware' => 'auth'], function () {
 
     //Please do not remove this if you want adminlte:route and adminlte:link commands to works correctly.
     #adminlte_routes
+    Route::get('prova1', 'Prova1Controller@index')->name('prova1');
+
+    Route::get('prova', 'ProvaController@index')->name('prova');
+
 });
